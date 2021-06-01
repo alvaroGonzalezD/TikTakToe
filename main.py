a19d5c050d74bccc9d9e16c104eaaad9ecdaa14c
Alvaro <aglez7412@gmail.com>
2021-06-01 20:29:38 +0200

    Agregadas 3 clases

---
+++ main.py
@@ -0,0 +1,17 @@
+class Tablero:
+    def __init__(self):
+        print("Tablero")
+
+
+class Jugador:
+    def __init__(self):
+        print("Jugador")
+
+
+class Juego:
+    def __init__(self):
+        print("Juego")
+
+Tablero()
+Jugador()
+Juego()
