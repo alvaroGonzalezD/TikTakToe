0f3530fabf4a99c7e798873da4c2e6d8ad689bcc
Alvaro <aglez7412@gmail.com>
2021-06-02 16:20:41 +0200

    Ahora ya tiene tablero y lo muestra

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 class Tablero:
     def __init__(self):
-        print("__init__ Tablero")
+        self.celdas = [
+            0, 0, 0, 
+            0, 0, 0, 
+            0, 0, 0]
+        print(self.celdas)
     
     def draw(self):
-        print("Dibujo Tablero")
+        print(f"""\t _______________________
+\t|       |       |       |
+\t|   {self.celdas[6]}   |   {self.celdas[7]}   |   {self.celdas[8]}   |
+\t|_______|_______|_______|
+\t|       |       |       |
+\t|   {self.celdas[3]}   |   {self.celdas[4]}   |   {self.celdas[5]}   |
+\t|_______|_______|_______|
+\t|       |       |       |
+\t|   {self.celdas[0]}   |   {self.celdas[1]}   |   {self.celdas[2]}   |
+\t|_______|_______|_______|""")
 
 
 class Jugador:
